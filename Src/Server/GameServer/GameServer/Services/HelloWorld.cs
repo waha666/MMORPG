8ab83fe7e3c0a3da6e831860d03b9372c422595f
waha666 <1981398103@qq.com>
2024-04-22 20:06:07 +0800

    feat:数据上传

---
+++ Src/Server/GameServer/GameServer/Services/HelloWorld.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Services
+{
+    internal class HelloWorld:Singleton<HelloWorld>
+    {
+        public void Init()
+        {
+           
+        }
+
+        public void Start()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<FirstTestRequest>(this.OnFirstTestRequest);
+        }
+
+        void OnFirstTestRequest(NetConnection<NetSession> sender, FirstTestRequest firstTest) 
+        {
+            Log.InfoFormat("OnFirstTestRequest HelloWorld:{0}", firstTest.Helloworld);
+        }
+    }
+}
